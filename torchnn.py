20fc3dfbb6102e633735cbd468ef61705d3fc097
Joshua <voydik@gmail.com>
2023-06-01 17:00:47 -0700

    remove 'cuda' + added images

--- torchnn.py
+++ torchnn.py
@@ -1,5 +1,4 @@
-# Import dependencies
-import torch 
+import torch
 from PIL import Image
 from torch import nn, save, load
 from torch.optim import Adam
@@ -7,57 +6,56 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
-# Get data 
+# Get data
 train = datasets.MNIST(root="data", download=True, train=True, transform=ToTensor())
 dataset = DataLoader(train, 32)
-#1,28,28 - classes 0-9
+# 1,28,28 - classes 0-9
 
 # Image Classifier Neural Network
-class ImageClassifier(nn.Module): 
+class ImageClassifier(nn.Module):
     def __init__(self):
         super().__init__()
         self.model = nn.Sequential(
-            nn.Conv2d(1, 32, (3,3)), 
+            nn.Conv2d(1, 32, (3, 3)),
             nn.ReLU(),
-            nn.Conv2d(32, 64, (3,3)), 
+            nn.Conv2d(32, 64, (3, 3)),
             nn.ReLU(),
-            nn.Conv2d(64, 64, (3,3)), 
+            nn.Conv2d(64, 64, (3, 3)),
             nn.ReLU(),
-            nn.Flatten(), 
-            nn.Linear(64*(28-6)*(28-6), 10)  
+            nn.Flatten(),
+            nn.Linear(64 * (28 - 6) * (28 - 6), 10)
         )
 
-    def forward(self, x): 
+    def forward(self, x):
         return self.model(x)
 
-# Instance of the neural network, loss, optimizer 
-clf = ImageClassifier().to('cuda')
+# Instance of the neural network, loss, optimizer
+clf = ImageClassifier()
 opt = Adam(clf.parameters(), lr=1e-3)
-loss_fn = nn.CrossEntropyLoss() 
+loss_fn = nn.CrossEntropyLoss()
 
-# Training flow 
-if __name__ == "__main__": 
-    for epoch in range(10): # train for 10 epochs
-        for batch in dataset: 
-            X,y = batch 
-            X, y = X.to('cuda'), y.to('cuda') 
-            yhat = clf(X) 
-            loss = loss_fn(yhat, y) 
+# Training flow
+if __name__ == "__main__":
+    for epoch in range(10):  # train for 10 epochs
+        for batch in dataset:
+            X, y = batch
+            yhat = clf(X)
+            loss = loss_fn(yhat, y)
 
-            # Apply backprop 
+            # Apply backprop
             opt.zero_grad()
-            loss.backward() 
-            opt.step() 
+            loss.backward()
+            opt.step()
 
         print(f"Epoch:{epoch} loss is {loss.item()}")
-    
-    with open('model_state.pt', 'wb') as f: 
-        save(clf.state_dict(), f) 
 
-    with open('model_state.pt', 'rb') as f: 
-        clf.load_state_dict(load(f))  
+    with open('model_state.pt', 'wb') as f:
+        save(clf.state_dict(), f)
 
-    img = Image.open('img_3.jpg') 
-    img_tensor = ToTensor()(img).unsqueeze(0).to('cuda')
+    with open('model_state.pt', 'rb') as f:
+        clf.load_state_dict(load(f))
+
+    img = Image.open('img_3.jpg')
+    img_tensor = ToTensor()(img).unsqueeze(0)
 
     print(torch.argmax(clf(img_tensor)))
